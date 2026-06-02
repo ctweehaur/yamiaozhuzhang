9c8eac984dfe61f3b5da8ec7b54129cb7cfa2167
ctweehaur <ctweehaur@gmail.com>
2026-06-02 16:04:06 +0800

    Create yamiaozhuzhang_translation.js

---
+++ yamiaozhuzhang_translation.js
@@ -0,0 +1,17 @@
+const translationData = [
+    {
+        zh: "宋国有一个人，担心他的禾苗长不快，于是就到田里把禾苗一棵一棵地往上拔。他疲惫不堪地回到家里，对家里人说：“今天可把我累坏了！我帮助禾苗长高了！”",
+        py: "sòng guó yǒu yī gè rén dān xīn tā de hé miáo zhǎng bú kuài yú shì jiù dào tián lǐ bǎ hé miáo yī kē yī kē de wǎng shàng bá tā pí bèi bù kān de huí dào jiā lǐ duì jiā lǐ rén shuō jīntiān kě bǎ wǒ lèi huài le wǒ bāng zhù hé miáo zhǎng gāo le",
+        en: "There was a man in the State of Song who felt anxious that his seedlings were not growing fast enough. So, he went into the field and pulled them up one by one. He returned home completely exhausted and told his family, 'I am worn out today! I helped the seedlings grow taller!'"
+    },
+    {
+        zh: "他的儿子听说后急忙快步跑到田里去看，发现禾苗却已经全都枯萎了。",
+        py: "tā de ér zǐ tīng shuō hòu jí máng kuài bù pǎo dào tián lǐ qù kàn fā xiàn hé miáo què yǐ jīng quán dōu kū wěi le",
+        en: "Upon hearing this, his son hurried to the field to check, only to find that all the seedlings had already withered."
+    },
+    {
+        zh: "普天之下，不去盲目帮助禾苗长高的人太少了！那些认为帮禾苗没有用处而放弃不管的人，就是不给禾苗锄草的人；而那些盲目违背规律去帮助禾苗生长的人，就是拔苗助长的人；这不仅没有好处，反而还害死了禾苗。",
+        py: "pǔ tiān zhī xià bú qù máng mù bāng zhù hé miáo zhǎng gāo de rén tài shǎo le nà xiē rèn wéi bāng hé miáo méi yǒu yòng chù ér fàng qì bù guǎn de rén jiù shì bù gěi hé miáo chú cǎo de rén ér nà xiē máng mù wéi bèng guī lǜ qù bāng zhù hé miáo shēng zhǎng de rén jiù shì bá miáo zhù zhǎng de rén zhè bù jǐn méi yǒu hǎo chù fǎn ér hái hài sǐ le hé miáo",
+        en: "In this world, those who do not foolishly interfere with the growth of seedlings are few indeed! Those who think it is useless and abandon them are like farmers who refuse to weed the fields; those who forcefully 'help' them grow are like this man pulling up seedlings. Such actions are not only useless, but they actually destroy what they intend to cultivate."
+    }
+];
